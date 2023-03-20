4813410da0dbd05a4c269a19168915614cefa150
Alex <zhestkov.82@mail.ru>
2023-03-20 15:06:14 +0400

    HW6

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+// Console.Write("Введите число M: ");
+// int m = int.Parse(Console.ReadLine() ?? "");
+// SequenceOfNumber sequenceofNumber = new SequenceOfNumber();
+// sequenceofNumber = InputSequence(m);
+// Console.WriteLine("{0} -> {1}", sequenceofNumber.getSequence(), sequenceofNumber.getCount());
+// Console.WriteLine(divider);
+
+
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+
+// Console.WriteLine("Первая прямая");
+// onsole.Write("Введите k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Вторая  прямая");
+// Console.Write("Введите k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());            
+// TLine line1 = new TLine(k1, b1);
+// TLine line2 = new TLine(k2, b2);
+// line1.Compare(line: line2);
+// Console.WriteLine(divider);
+// Console.ReadKey();
